6cb75af1aa99c41dc8d0c3a877ae0ea7a646b0fd
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-19 16:37:43 +0530

    Create primary_sm_btn.blade.php

---
+++ resources/views/components/primary_sm_btn.blade.php
@@ -0,0 +1,7 @@
+<button type="submit" class="btn btn-flat primary_btn float-right navbar-orange">{{$name}}</button>
+<style>
+    .primary_btn:hover{
+        background-color: #007bff;
+    }
+
+</style>
