8ab5f90861e932a9cb90662f3a8b8f499becb889
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-19 16:36:18 +0530

    Create 2022_02_19_065709_create_months_table.php

---
+++ database/migrations/2022_02_19_065709_create_months_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMonthsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('months', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('classe_id');
+            $table->foreign('classe_id')->references('id')->on('classes');
+            $table->string('name');
+            $table->double('fee');
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('months');
+    }
+}
