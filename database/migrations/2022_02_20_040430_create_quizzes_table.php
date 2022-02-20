ff785f38c20a9a9b539e8ec7d7e8f158d2c1295a
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-20 12:31:21 +0530

    Create 2022_02_20_040430_create_quizzes_table.php

---
+++ database/migrations/2022_02_20_040430_create_quizzes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('month_id');
+            $table->foreign('month_id')->references('id')->on('months');
+            $table->string('name');
+            $table->longText('description');
+            $table->longText('embed_code');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
