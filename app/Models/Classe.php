c69ab9a2250eca81eb21deff694470d3e4f9f71e
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-20 12:30:53 +0530

    Update Classe.php

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Classe extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
     protected $fillable = [
         'name',
         'url'
     ];
 
-    public function months(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function months(): HasMany
     {
         return $this->hasMany(Month::class);
     }
