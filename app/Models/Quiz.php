3881fd406b816869fd7c7875da47ca3dd5b7aaba
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-20 12:31:01 +0530

    Create Quiz.php

---
+++ app/Models/Quiz.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Quiz extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'embed_code',
+        'month_id'
+    ];
+
+    public function month(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Month::class);
+    }
+}
