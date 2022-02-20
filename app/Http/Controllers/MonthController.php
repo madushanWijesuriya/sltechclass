89e1384595168779d765cef6f1b332119bdd5490
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-20 12:30:31 +0530

    Update MonthController.php

--- app/Http/Controllers/MonthController.php
+++ app/Http/Controllers/MonthController.php
@@ -2,22 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Service\ToastMessageServices;
 use App\Models\Classe;
 use App\Models\Month;
 use Carbon\Carbon;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Nette\Schema\ValidationException;
 
 class MonthController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -28,40 +32,40 @@ class MonthController extends Controller
      * Show the form for creating a new resource.
      *
      * @param $class_id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @return Application|Factory|View
      */
     public function createMonth($id)
     {
-        return view('months.create',compact('id'));
+        return view('months.create', compact('id'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
     public function store(Request $request)
     {
         $validate = Validator::make($request->all(), [
             'name' => ['required', 'string'],
             'fee' => ['required'],
-            'class_id' => ['required','exists:classes,id'],
+            'class_id' => ['required', 'exists:classes,id'],
             'period' => ['required'],
         ]);
         $class = Classe::find($request->input('class_id'));
         //after validate
-        if ($class->months()->where('name',$request->input('name'))->exists())
-            return redirect()->back()->with(ToastMessageServices::generateMessage('Name is Already Taken',false));
+        if ($class->months()->where('name', $request->input('name'))->exists())
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Name is Already Taken', false));
 
         //get message type
         $notification = ToastMessageServices::generateValidateMessage($validate);
         //check message type and return message
         if ($notification !== true)
             return redirect()->back()->with($notification);
-        try{
+        try {
 
-            $dates = explode(' - ' ,$request->input('period'));
+            $dates = explode(' - ', $request->input('period'));
 
             $class->months()->create([
                 'fee' => (double)$request->input('fee'),
@@ -70,10 +74,9 @@ class MonthController extends Controller
                 'end_at' => Carbon::parse($dates[1])->endOfDay(),
             ]);
 
-            return redirect()->back()->with(ToastMessageServices::generateMessage('successfully added'));
+            return redirect()->route('class.edit', $class->id)->with(ToastMessageServices::generateMessage('successfully added'));
 
-        }catch(\Exception $e){
-            dd($e->getMessage());
+        } catch (Exception $e) {
             DB::rollBack();
             return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Added', false));
 
@@ -84,8 +87,8 @@ class MonthController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
@@ -95,21 +98,21 @@ class MonthController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @param int $id
+     * @return Application|Factory|View
      */
     public function edit($id)
     {
         $month = Month::find($id);
-        return view('months.edit',compact('month'));
+        return view('months.edit', compact('month'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return RedirectResponse
      */
     public function update(Request $request, $id)
     {
@@ -120,18 +123,18 @@ class MonthController extends Controller
         $month = Month::find($id);
         $class = $month->classe;
         //after validate
-        if ($class->months()->where('name',$request->input('name'))->exists() && $month->name !== $request->input('name'))
-            return redirect()->back()->with(ToastMessageServices::generateMessage('Name is Already Taken',false));
+        if ($class->months()->where('name', $request->input('name'))->exists() && $month->name !== $request->input('name'))
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Name is Already Taken', false));
 
         //get message type
         $notification = ToastMessageServices::generateValidateMessage($validate);
         //check message type and return message
         if ($notification !== true)
             return redirect()->back()->with($notification);
-        try{
+        try {
 
             if ($request->input('period')) {
-                $dates = explode(' - ' ,$request->input('period'));
+                $dates = explode(' - ', $request->input('period'));
                 $start = Carbon::parse($dates[0])->startOfDay();
                 $end = Carbon::parse($dates[1])->endOfDay();
             } else {
@@ -145,9 +148,9 @@ class MonthController extends Controller
                 'end_at' => $end,
             ]);
 
-            return redirect()->back()->with(ToastMessageServices::generateMessage('successfully added'));
+            return redirect()->route('class.edit', $month->classe->id)->with(ToastMessageServices::generateMessage('successfully added'));
 
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             DB::rollBack();
             return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Added', false));
 
@@ -158,12 +161,19 @@ class MonthController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return RedirectResponse
      */
-    public function destroy($id)
+    public function deleteMonth($id)
     {
-        dd($id
-        );
+        try {
+
+            if (Month::find($id)->delete())
+                return redirect()->back()->with(ToastMessageServices::generateMessage('successfully deleted'));
+
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Delete', false));
+        } catch (Exception $e) {
+            return redirect()->back()->with(ToastMessageServices::generateMessage($e->getMessage(), false));
+        }
     }
 }
