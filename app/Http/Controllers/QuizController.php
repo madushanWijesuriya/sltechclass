5c379144456da73a4aea9c991ce4fecd4999cb32
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-02-20 12:30:41 +0530

    Update QuizController.php

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Service\ToastMessageServices;
+use App\Models\Month;
+use App\Models\Quiz;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class QuizController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -20,29 +29,51 @@ class QuizController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @return Application|Factory|View
      */
     public function createQuiz($id)
     {
-        return view('quiz.create',compact('id'));
+        return view('quiz.create', compact('id'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $validate = Validator::make($request->all(), [
+            'name' => ['required', 'string'],
+            'embed_code' => ['required', 'string'],
+            'month_id' => ['required', 'exists:months,id'],
+            'description' => ['required', 'string'],
+        ]);
+        //get message type
+        $notification = ToastMessageServices::generateValidateMessage($validate);
+        //check message type and return message
+        if ($notification !== true)
+            return redirect()->back()->with($notification);
+
+        try {
+
+            $month = Month::find($request->input('month_id'));
+            if ($month->quizzes()->create($request->only(['name', 'description', 'embed_code'])))
+                return redirect()->route('class.edit', $month->classe->id)->with(ToastMessageServices::generateMessage('successfully added'));
+
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Added', false));
+
+        } catch (Exception $e) {
+            return redirect()->back()->with(ToastMessageServices::generateMessage($e->getMessage(), false));
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
@@ -52,34 +83,64 @@ class QuizController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function edit($id)
     {
-        //
+        $quiz = Quiz::find($id);
+        return view('quiz.edit', compact('quiz'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return RedirectResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $validate = Validator::make($request->all(), [
+            'name' => ['required', 'string'],
+            'embed_code' => ['required', 'string'],
+            'description' => ['required', 'string'],
+        ]);
+        //get message type
+        $notification = ToastMessageServices::generateValidateMessage($validate);
+        //check message type and return message
+        if ($notification !== true)
+            return redirect()->back()->with($notification);
+
+        try {
+
+            $quiz = Quiz::find($id);
+
+            if ($quiz->update($request->only(['name', 'description', 'embed_code'])))
+                return redirect()->route('class.edit', $quiz->month->classe->id)->with(ToastMessageServices::generateMessage('successfully updated'));
+
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Update', false));
+
+        } catch (Exception $e) {
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Update', false));
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return RedirectResponse
      */
-    public function destroy($id)
+    public function deleteQuiz($id)
     {
-        //
+        try {
+            if (Quiz::find($id)->delete())
+                return redirect()->back()->with(ToastMessageServices::generateMessage('successfully deleted'));
+
+            return redirect()->back()->with(ToastMessageServices::generateMessage('Cannot Delete', false));
+        } catch (Exception $e) {
+            return redirect()->back()->with(ToastMessageServices::generateMessage($e->getMessage(), false));
+        }
     }
 }
