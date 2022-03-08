b04ea907d5f9a7d4ddc5d5bd602122a9e69eb22a
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-03-08 23:47:43 +0530

    Update Student.php

--- app/Http/Middleware/Student.php
+++ app/Http/Middleware/Student.php
@@ -4,20 +4,23 @@ namespace App\Http\Middleware;
 
 use App\Http\Service\ToastMessageServices;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Session;
 
 class Student
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->user()->type !== "student"){
+        if (auth()->user()->type !== "student") {
             return redirect()->back()->with(ToastMessageServices::generateMessage('Not Allowed', false));
         }
         return $next($request);
