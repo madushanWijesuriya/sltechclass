3bd9ce57b3a7b4b0ac7d9e3efdf927c1d8ffbc1d
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-03-08 23:49:58 +0530

    Update index.blade.php

--- resources/views/StudentPortal/classes/index.blade.php
+++ resources/views/StudentPortal/classes/index.blade.php
@@ -19,34 +19,55 @@
         <div class="container-fluid">
             <div class="row">
                 @if($classes)
-                    @foreach($classes as $index => $class)
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            @if($index === 0)
-                                <div class="small-box bg-info">
-                                    @elseif($index === 1)
-                                        <div class="small-box bg-success">
-                                            @elseif($index === 2)
-                                                <div class="small-box bg-danger">
-                                                    @endif
-                                                    <div class="inner">
-                                                        <h3>Name - {{$class->name}}</h3>
-                                                    </div>
-                                                    <div class="icon">
-                                                        <i class="ion ion-bag"></i>
-                                                    </div>
-                                                    <a href="{{route('student-class.show',$class->id)}}"
-                                                       class="small-box-footer">Go Class<i
-                                                            class="fas fa-arrow-circle-right"></i></a>
-                                                </div>
+                    @foreach($classes as $class)
+                        <div class="col-lg-6 col-sm-6 col-md-6">
+                            <a href="{{route('student-class.show',$class->id)}}">
+                                <figure class="figure">
+                                    <img width="200px" height="100px" src="{{asset('/class_thumbnails/'.$class->url)}}" loading="lazy">
+                                    <div class="row">
+                                        <div class="col">
+                                            <figcaption class="figure-caption text-center">{{$class->name}}</figcaption>
                                         </div>
-                                        @endforeach
-                                    @else
-                                    @endif
-
-                                </div>
+                                    </div>
+                                </figure>
+                            </a>
                         </div>
+                    @endforeach
+                @else
+                @endif
+            </div>
+        </div>
     </section>
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Payments</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    @include('StudentPortal.payments.delay.table')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Payments History</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    @include('StudentPortal.payments.history.dashboard_table')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Announcement</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    @include('StudentPortal.Announcement.dashboard_table')
 @endsection
 <style>
     .formHeader {
