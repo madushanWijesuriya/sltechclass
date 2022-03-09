a35654179d3fa537e70e02d0e7bf42c4c4560e86
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-03-09 22:01:56 +0530

    Update primary_btn.blade.php

--- resources/views/components/primary_btn.blade.php
+++ resources/views/components/primary_btn.blade.php
@@ -1,7 +1,14 @@
-<button @if(isset($form)) form="{{$form}}" @endif  id="{{isset($id) ? $id : "submit"}}" type="{{isset($type) ? $type : "submit"}}" class="{{isset($class_name) ? $class_name : "btn btn-flat primary_btn float-right navbar-orange"}}">{{isset($name) ? $name : "Submit"}}</button>
+<button @if(isset($form)) form="{{$form}}" @endif  id="{{isset($id) ? $id : "submit"}}"
+        type="{{isset($type) ? $type : "submit"}}"
+        class="{{isset($class_name) ? $class_name : "btn btn-flat primary_btn float-right navbar-orange"}}">{{isset($name) ? $name : "Submit"}}</button>
 <style>
-    .primary_btn:hover{
+    .primary_btn:hover {
         background-color: #007bff;
     }
-
+    .secondary_btn:hover{
+        background-color: #ff7700;
+    }
+    .navbar-blue{
+        background-color: #007bff;
+    }
 </style>
