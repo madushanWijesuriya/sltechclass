747b783d79796c47e6838b5ad66554259ec75c38
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-03-09 22:02:07 +0530

    Update direct_bank_checkout.blade.php

--- resources/views/StudentPortal/direct_bank_checkout.blade.php
+++ resources/views/StudentPortal/direct_bank_checkout.blade.php
@@ -26,38 +26,88 @@
                             <h3 class="card-title">Direct Bank Transfer</h3>
                         </div>
                         <div class="card-body">
-                            <div class="col-6">
-{{--                                <img src="{{'/dist/img/credit/visa.png'}}" alt="Visa">--}}
-{{--                                <img src="{{'/dist/img/credit/mastercard.png'}}" alt="Mastercard">--}}
-{{--                                <img src="{{'/dist/img/credit/american-express.png'}}" alt="American Express">--}}
-{{--                                <img src="{{'/dist/img/credit/paypal2.png'}}" alt="Paypal">--}}
+                            <div class="container">
+                                <div class="row" style="text-align: center">
+                                    <b>
+                                        පහත විස්තර අනුව ඔබගේ ගෙවීම් කටයුතු සිදු කරන්න
+                                    </b>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-sm-0 col-lg-2 col-md-2"></div>
+                                    <div class="col-sm-12 col-lg-8 col-md-10">
+                                        <div class="card-body" style="background-color: rgba(237,245,234,0.95)">
+                                            <div class="row">
+                                                <div class="col-sm-2 col-lg-4 col-md-3">
+                                                    <img width="120px" height="40px" src="{{asset('/8.png')}}"
+                                                         loading="lazy">
+                                                </div>
+                                                <div class="col-sm-0 col-lg-1 col-md-2"></div>
+                                                <div class="col-sm-10 col-lg-7 col-md-7">
+                                                    <b>Card Number - 5333 1711 2598 2484</b><br>
+                                                    <b>Codici Fiscale - MNTDSH89A09Z209M</b>
+                                                </div>
 
-                                <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">ඔබේ ගෙවීම තහවුරු කිරිමට ගෙවීම් පත්‍රයේ පින්තූරයක් upload කර Submit බොත්තම ඔබන්න</p>
-                                සැ.‍යු: ඔබේ ගෙවීම තහවුරු වූ පසු ඔබට පන්තිය සමග සම්බන්ද විය හැක.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-0 col-lg-2 col-md-2"></div>
 
+                                </div>
+                                <br>
+                                <div class="row" style="text-align:center">
+                                    <b>
+                                        ගෙවීම් සිදු කිරීමේදී වැඩි දුර ගෙවීම පිලිබද තොරතුරු අවශ්‍ය නම් අපව whatsapp
+                                        මාර්ගයෙන් සම්භන්ධ කර ගන්න ​
+                                    </b>
+                                </div>
+                                <br>
+
+                                <div class="row" style="text-align:center">
+                                    <b>
+                                        ඔබ සිදු කල ගෙවීමේ රිසිට් පත උපරිම දවස් 02ක් ඇතුලත අපවෙත එවන්න, ගෙවීම් කල දින සිට
+                                        දවස් 02 ඉක්මවූ විට ගෙවීම වලංගු නොවන බව මතක තබා ගන්න ! ​
+                                    </b>
+                                </div>
+                                <br>
+
+                                <div class="row" style="text-align:center">
+                                    <b>
+                                        ඔබ සිදු කල ගෙවීමේ රිසිට් පත පහතින් අපවෙත එවන්න ( Upload )​
+                                    </b>
+                                </div>
+                                <br>
 
                             </div>
                             <br>
-                            <form id="directForm" action="{{route('directPay')}}" method="post"
-                                  enctype="multipart/form-data">
-                                @csrf
-                                <input type="hidden" name="order_id" value="{{$data["order_id"]}}">
-                                <div class="form-group">
-                                    <label for="url">බොත්තම ඔබන්න පෙර image එක upload කරන්න:</label>
-                                    <span style="color:#ff0000"></span>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" name="url" id="url" required>
+                            <br>
+                            <div class="row" style="text-align:center">
+                                <form id="directForm" action="{{route('directPay')}}" method="post"
+                                      enctype="multipart/form-data">
+                                    @csrf
+                                    <input type="hidden" name="order_id" value="{{$data["order_id"]}}">
+                                    <div class="form-group">
+                                        <label for="url">බොත්තම ඔබන්න පෙර image එක upload කරන්න:</label>
+                                        <span style="color:#ff0000"></span>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" name="url" id="url" required>
+                                            </div>
                                         </div>
                                     </div>
+                                </form>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-12 col-md-12 col-lg-6">
+                                    @include('components.primary_btn',['is_white'=>true,'form'=>'directForm' ,'name' => "Direct Pay", "id"=>"direct",'class_name'=>'btn btn-flat primary_btn float-right navbar-blue text-white secondary_btn'])
                                 </div>
-                                <div class="row">
-                                    <div class="col-lg-11 col-sm-12 col-md-8"></div>
-                                    <div class="col-lg-1 col-sm-12 col-md-4">
-                                        @include('components.primary_btn',['form'=>'directForm' ,'name' => "Direct Pay", "id"=>"direct"])
-                                    </div>
-                                </div>
-                            </form>
+                            </div>
+                            <br>
+                            <br>
+                            <br>
+
+
                         </div>
 
                     </div>
