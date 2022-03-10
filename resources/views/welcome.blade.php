24a7117601c7741a1440ec7fa1dd3702494fc42f
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-03-10 19:15:28 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,12 +22,12 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('admin.login'))
+            @if (Route::has('student.login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ route('student-class.index') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
                     @else
-                        <a href="{{ route('admin.login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href="{{ route('student.login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
                     @endauth
                 </div>
             @endif
