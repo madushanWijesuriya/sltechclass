e4948a70827ffb5e6f52557672dd3ba620deb529
madushanWijesuriya <shanwijesuriya.madushan@gmail.com>
2022-03-10 19:06:33 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,11 +85,6 @@
 </aside>
 
 <div class="wrapper">
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60"
-             width="60">
-    </div>
     @endauth
     <div class="content-wrapper">
         {{--        @if (Session::has('status'))--}}
